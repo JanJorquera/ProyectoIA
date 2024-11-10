6f3a492b8e52fade0259b707831891031249f4a5
JanJorquera <janjorquera@gmail.com>
2024-11-09 21:08:43 -0300

    Avance

--- main.cpp
+++ main.cpp
@@ -431,6 +431,7 @@ void replaceHotel(individuo *hijo){
     listaHotelesDAux = listaHotelesD;
     sizeAux = listaHotelesDAux.size();
     for (size_t j=0; j<sizeAux; j++){
+      cout << "j: " << j << endl;
       posHotel = int_rand(0,listaHotelesDAux.size());
       if (t[getPos(listaHotelesTour[i-1])][getPos(listaHotelesDAux[posHotel])] <= T[i-1] && t[getPos(listaHotelesDAux[posHotel])][getPos(listaHotelesTour[i+1])] <= T[i]){
         flagHotelSwap = true;
