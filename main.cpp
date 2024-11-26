f82b874f73e2652f67777e1c732447f973075734
JanJorquera <janjorquera@gmail.com>
2024-11-26 18:50:55 -0300

    version final AG-OPHS

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 bool debug = false;
 bool debugOpt = false;
 bool debugGenerateFeasibleSequences = false;
-bool debugTime = true;
+bool debugTime = false;
 
 // Parámetros del problema
 int N, HP1, D;
